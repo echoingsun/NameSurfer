9617e61a533f773b1cb22887b0f877ecec98812d
S <s>
2017-03-05 14:44:40 -0800

    {"types":"C","runs":5}

--- testtest.java
+++ testtest.java
@@ -6,7 +6,7 @@ public class testtest extends ConsoleProgram implements NameSurferConstants {
 	public void run(){
 		NameSurferEntry test = new NameSurferEntry ("sam 10 12 13 141 15 16 17 18 19 300 29");
 		println (test.toString());
-		println (test.getName());
-		println (test.getRank(2000));
+		//println (test.getName());
+		//println (test.getRank(2000));
 	}
 }
