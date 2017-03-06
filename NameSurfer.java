7afe985b999a44f0c35e0f0016062dbca6608c1a
S <s>
2017-03-05 17:59:48 -0800

    {"types":"C","runs":31}

--- NameSurfer.java
+++ NameSurfer.java
@@ -45,7 +45,7 @@ public class NameSurfer extends Program implements NameSurferConstants {
 private void loadGraph() {
 	graph = new NameSurferGraph();
 	add(graph);
-	graph.drawBackground();
+	graph.drawBackground(nameLabel.getHeight());
 	
 }
 
