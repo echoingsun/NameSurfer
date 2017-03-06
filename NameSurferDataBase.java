e814ed405294706c62c4c13f102845a90ce67649
S <s>
2017-03-05 23:12:06 -0800

    {"types":"C","runs":80}

--- NameSurferDataBase.java
+++ NameSurferDataBase.java
@@ -1,3 +1,4 @@
+
 /*
  * File: NameSurferDataBase.java
  * -----------------------------
@@ -17,46 +18,54 @@ import java.io.IOException;
 import java.util.*;
 
 public class NameSurferDataBase implements NameSurferConstants {
-	
-	HashMap <String, String> dataBase = new HashMap <String, String>();
-	
-/* Constructor: NameSurferDataBase(filename) */
-/**
- * Creates a new NameSurferDataBase and initializes it using the
- * data in the specified file.  The constructor throws an error
- * exception if the requested file does not exist or if an error
- * occurs as the file is being read.
- */
+
+	// The database is actually a HashMap -
+	// It reads the data from the file and stores them into the HashMap.
+	HashMap<String, String> dataBase = new HashMap<String, String>();
+
+	/*
+	 * The constructor creates a new NameSurferDataBase and initializes it using
+	 * the data in the specified file. The constructor throws an error exception
+	 * if the requested file does not exist or if an error occurs as the file is
+	 * being read.
+	 */
 	public NameSurferDataBase(String filename) {
-		try{
+
+		// Read file
+		try {
 			BufferedReader br = new BufferedReader(new FileReader(filename));
-			while (true){
+			while (true) {
 				String line = br.readLine();
-				if (line == null) break;
-				NameSurferEntry newLine = new NameSurferEntry (line);
+				if (line == null)
+					break;
+
+				// For each lind read, generate a new entry
+				// and put it in the hashmap.
+				NameSurferEntry newLine = new NameSurferEntry(line);
 				dataBase.put(newLine.getName().toLowerCase(), newLine.toString());
-				}		
+			}
 			br.close();
-		} catch (IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-/* Method: findEntry(name) */
-/**
- * Returns the NameSurferEntry associated with this name, if one
- * exists.  If the name does not appear in the database, this
- * method returns null.
- */
+
+	/*
+	 * Returns the NameSurferEntry associated with this name, if one exists. If
+	 * the name does not appear in the database, this method returns null.
+	 */
 	public NameSurferEntry findEntry(String name) {
 		String entry = "";
-		if (dataBase.containsKey(name.toLowerCase())){
+		if (dataBase.containsKey(name.toLowerCase())) {
+			
+			// Get the element associated with the name.
 			entry = dataBase.get(name.toLowerCase());
+			
+			// Create the entry using the string.
 			NameSurferEntry newEntry = new NameSurferEntry(entry);
 			return newEntry;
 		}
 		return null;
-		
+
 	}
 }
-
