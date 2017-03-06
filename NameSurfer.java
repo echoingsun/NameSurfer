18cafa6e6fac8123f20b8e20f3fbc22da484b38a
S <s>
2017-03-05 21:59:20 -0800

    {"types":"C","runs":79}

--- NameSurfer.java
+++ NameSurfer.java
@@ -1,3 +1,4 @@
+
 /*
  * Name: Rachel Sun
  * File: NameSurfer.java
@@ -12,84 +13,81 @@ import javax.swing.*;
 
 public class NameSurfer extends Program implements NameSurferConstants {
 
-	// Define some interactors as instance variables.
-	private JLabel nameLabel = new JLabel ("Name");
-	private JTextField textField = new JTextField (TF_LEN);
-	private JButton graphButton = new JButton ("Graph");
-	private JButton clearButton = new JButton ("Clear");
-	private NameSurferDataBase namesData;
-	private NameSurferGraph graph;
+	// Define interactors as instance variables.
+	private JLabel nameLabel = new JLabel("Name");
+	private JTextField textField = new JTextField(TF_LEN);
+	private JButton graphButton = new JButton("Graph");
+	private JButton clearButton = new JButton("Clear");
 	
-/* Method: init() */
-/**
- * This method has the responsibility for reading in the data base
- * and initializing the interactors at the top of the window.
- */
+	// Define the database to be used.
+	private NameSurferDataBase namesData;
+	
+	// Define the canvas to display data.
+	private NameSurferGraph graph;
+
 	public void init() {
-		
+
+		// Create a database and load the canvas.
 		loadDatabase();
 		loadGraph();
-		
-		add (nameLabel, NORTH);
-	    add (textField, NORTH);
-	    add (graphButton, NORTH);
-	    add (clearButton, NORTH);
-	    
-	    addActionListeners();  
 
-	    // http://stackoverflow.com/questions/25007113/pressing-jbutton-with-the-enter-key
-	    textField.addKeyListener(new KeyAdapter() {
-	    	@Override
-	    	public void keyPressed (KeyEvent e){
-	    		if (e.getKeyCode() == KeyEvent.VK_ENTER){
-	    			String nameEntry = textField.getText();
-	    			if (namesData.findEntry(nameEntry) != null){
-	    				NameSurferEntry newInquiry = new NameSurferEntry (namesData.findEntry(nameEntry).toString());
-	    				graph.addEntry(newInquiry);
-	    		}
-	    	}
-	    }
-	    	
-	    });
-	    
-	    
-	   
+		// Add interactors to the application.
+		add(nameLabel, NORTH);
+		add(textField, NORTH);
+		add(graphButton, NORTH);
+		add(clearButton, NORTH);
+
+		addActionListeners();
+
+		// For the textField, add keyListener that responds to pressing enter.
+		// Reference:
+		// http://stackoverflow.com/questions/25007113/pressing-jbutton-with-the-enter-key
+		textField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					
+					// Detailed comments see method ActionPerformed
+					String nameEntry = textField.getText();
+					if (namesData.findEntry(nameEntry) != null) {
+						NameSurferEntry newInquiry = new NameSurferEntry(namesData.findEntry(nameEntry).toString());
+						graph.addEntry(newInquiry);
+					}
+				}
+			}
+		});
+
 	}
-	
-	public void run() {
-		
+
+/*	public void run() {
+
+	}*/
+
+	private void loadGraph() {
+		graph = new NameSurferGraph();
+		add(graph);
+
 	}
-	
 
-	
+	private void loadDatabase() {
+		namesData = new NameSurferDataBase(NAMES_DATA_FILE);
 
+	}
 
-private void loadGraph() {
-	graph = new NameSurferGraph();
-	add(graph);
-	
-}
-
-private void loadDatabase() {
-	namesData = new NameSurferDataBase(NAMES_DATA_FILE);
-	
-}
-
-/* Method: actionPerformed(e) */
-/**
- * This class is responsible for detecting when the buttons are
- * clicked, so you will have to define a method to respond to
- * button actions.
- */
+	/* Method: actionPerformed(e) */
+	/**
+	 * This class is responsible for detecting when the buttons are clicked, so
+	 * you will have to define a method to respond to button actions.
+	 */
 	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand().equals("Graph")){
+		if (e.getActionCommand().equals("Graph")) {
 			String nameEntry = textField.getText();
-			if (namesData.findEntry(nameEntry) != null){
-				NameSurferEntry newInquiry = new NameSurferEntry (namesData.findEntry(nameEntry).toString());
+			if (namesData.findEntry(nameEntry) != null) {
+				NameSurferEntry newInquiry = new NameSurferEntry(namesData.findEntry(nameEntry).toString());
 				graph.addEntry(newInquiry);
-			}			
+			}
 		}
-		if (e.getActionCommand().equals("Clear")){
+		if (e.getActionCommand().equals("Clear")) {
 			graph.clear();
 		}
 	}
